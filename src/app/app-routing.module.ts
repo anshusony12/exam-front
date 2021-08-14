ff3bc89e3afc29be9e18a7e420c71dc224f7a166
Anshu Sony <48364174+anshusony12@users.noreply.github.com>
2021-08-14 14:12:54 +0530

    commit for Login design completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import {SignupComponent} from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path:'',
+      component: HomeComponent
+    },
+    {
+      path:'signup',
+      component: SignupComponent,
+      pathMatch: 'full'
+    },
+    {
+      path:'login',
+      component: LoginComponent,
+      pathMatch: 'full'
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
