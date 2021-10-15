0c5fb80d3ab6960dcf7f30eca4cf5210bfdea679
Anshu Sony <48364174+anshusony12@users.noreply.github.com>
2021-10-15 22:38:01 +0530

    final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCategoryComponent } from './pages/admin/add-category/add-category.component';
+import { AddQuestionComponent } from './pages/admin/add-question/add-question.component';
+import { AddQuizComponent } from './pages/admin/add-quiz/add-quiz.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { UpdateQuestionComponent } from './pages/admin/update-question/update-question.component';
+import { UpdateQuizComponent } from './pages/admin/update-quiz/update-quiz.component';
+import { ViewCategoriesComponent } from './pages/admin/view-categories/view-categories.component';
+import { ViewQuestionsComponent } from './pages/admin/view-questions/view-questions.component';
+import { ViewQuizzesComponent } from './pages/admin/view-quizzes/view-quizzes.component';
+import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import {SignupComponent} from './pages/signup/signup.component';
+import { InstructionsComponent } from './pages/user/instructions/instructions.component';
+import { LoadQuizzesComponent } from './pages/user/load-quizzes/load-quizzes.component';
+import { StartComponent } from './pages/user/start/start.component';
+import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
+import { AdminGuard } from './services/admin.guard';
+import { NormalGuard } from './services/normal.guard';
 
 const routes: Routes = [
     {
@@ -18,6 +35,73 @@ const routes: Routes = [
       path:'login',
       component: LoginComponent,
       pathMatch: 'full'
+    },
+    {
+      path:'admin',
+      component: DashboardComponent,
+      canActivate: [AdminGuard],
+      children:[
+        {
+          path:'',
+          component: WelcomeComponent,
+        },
+        {
+          path:'profile',
+          component: ProfileComponent,
+        },
+        {
+          path:'categories',
+          component:ViewCategoriesComponent
+        },
+        {
+          path:'add-category',
+          component:AddCategoryComponent
+        },
+        {
+          path:'quizzes',
+          component:ViewQuizzesComponent
+        },
+        {
+          path:'add-quiz',
+          component:AddQuizComponent
+        },
+        {
+          path:'quiz/:qid',
+          component:UpdateQuizComponent
+        },
+        {
+          path:'view-questions/:qid/:title',
+          component:ViewQuestionsComponent
+        },
+        {
+          path:'add-question/:qid/:title',
+          component:AddQuestionComponent
+        },
+        {
+          path:'update-question/:quesId',
+          component:UpdateQuestionComponent
+        }
+      ]
+    },
+    {
+      path:'user-dashboard',
+      component: UserDashboardComponent,
+      canActivate: [NormalGuard],
+      children:[
+        {
+          path:':catId',
+          component:LoadQuizzesComponent
+        },
+        {
+          path:'instructions/:qid',
+          component:InstructionsComponent
+        },        
+      ]
+    },
+    {
+      path:'start/:qid',
+      component:StartComponent,
+      canActivate: [NormalGuard]
     }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,30 @@ import { HomeComponent } from './pages/home/home.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { authInterceptorProvider } from './services/auth.interceptor';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import {MatListModule} from '@angular/material/list';
+import { SidebarComponent } from './pages/admin/sidebar/sidebar.component';
+import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
+import { ViewCategoriesComponent } from './pages/admin/view-categories/view-categories.component';
+import { AddCategoryComponent } from './pages/admin/add-category/add-category.component';
+import { ViewQuizzesComponent } from './pages/admin/view-quizzes/view-quizzes.component';
+import { AddQuizComponent } from './pages/admin/add-quiz/add-quiz.component';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
+import { UpdateQuizComponent } from './pages/admin/update-quiz/update-quiz.component';
+import { ViewQuestionsComponent } from './pages/admin/view-questions/view-questions.component';
+import { AddQuestionComponent } from './pages/admin/add-question/add-question.component';
+import { UpdateQuestionComponent } from './pages/admin/update-question/update-question.component';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { SidebarComponent as UserSideBar } from './pages/user/sidebar/sidebar.component';
+import { LoadQuizzesComponent } from './pages/user/load-quizzes/load-quizzes.component';
+import { InstructionsComponent } from './pages/user/instructions/instructions.component';
+import { StartComponent } from './pages/user/start/start.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { NgxUiLoaderHttpModule, NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @NgModule({
   declarations: [
@@ -26,7 +50,24 @@ import {MatIconModule} from '@angular/material/icon';
     FooterComponent,
     SignupComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    DashboardComponent,
+    UserDashboardComponent,
+    ProfileComponent,
+    SidebarComponent,
+    WelcomeComponent,
+    ViewCategoriesComponent,
+    AddCategoryComponent,
+    ViewQuizzesComponent,
+    AddQuizComponent,
+    UpdateQuizComponent,
+    ViewQuestionsComponent,
+    AddQuestionComponent,
+    UpdateQuestionComponent,
+    UserSideBar,
+    LoadQuizzesComponent,
+    InstructionsComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
@@ -40,9 +81,16 @@ import {MatIconModule} from '@angular/material/icon';
     MatSnackBarModule,
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    CKEditorModule,
+    MatProgressSpinnerModule,
+    NgxUiLoaderModule,
+    NgxUiLoaderHttpModule.forRoot({ showForeground: true }),
   ],
-  providers: [],
+  providers: [authInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
